Fix the image normalization bug and update the model link

promptda/promptda.py
@@ -89,7 +89,7 @@ class PromptDA(nn.Module):
         prompt_depth, min_val, max_val = self.normalize(prompt_depth)
         h, w = x.shape[-2:]
         features = self.pretrained.get_intermediate_layers(
-            x, self.model_config['layer_idxs'],
+            (x - self._mean) / self._std, self.model_config['layer_idxs'],
             return_class_token=True)
         patch_h, patch_w = h // self.patch_size, w // self.patch_size
         depth = self.depth_head(features, patch_h, patch_w, prompt_depth)